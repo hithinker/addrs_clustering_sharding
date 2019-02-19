e4992f5063399ee095f729bce7d7ddc3265b819a
MaoZhilai <zhlmao17@gmail.com>
2019-02-19 21:37:30 +0800

    add file path!!

--- src/com/btc/paper/data/BlockProcessor.java
+++ src/com/btc/paper/data/BlockProcessor.java
@@ -14,23 +14,27 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class BlockProcessor {
-	public HashMap<int[],Float> readBlock(String dir, int round) throws Exception {
+	public HashMap<int[],Float> readBlock(String dir, int round){
 		File blockDir = new File(dir);
 		StringBuilder sb = new StringBuilder();
 		JSONObject block_json_obj = null;
+		//划分地址簇前后对比
 		ArrayList<Integer> beforeClusteredShardStat = new ArrayList<Integer>();
 		ArrayList<Integer> afterClusteredShardStat = new ArrayList<Integer>();
+		//addr-id Map
 		HashMap<String,Integer> addr_id = new HashMap<String,Integer>();
-		HashMap<Integer,ArrayList<Float>> epochEdges = new HashMap<Integer,ArrayList<Float>>();
-		int idCounter = 0;
 		int addrsCount = 0;
+		int epochAddrCount = 0;
+		// id-cid 映射关系图
 	    HashMap<Integer,Integer> id_cid = null;
+	    //start 时间
 		long start = System.currentTimeMillis();
 		if(round > 0) {
-			addr_id = this.getAddrIdMap("");
+			addr_id = this.getAddrIdMap("/home/infosec/sharding_expt/addrid.txt");
 			addrsCount = addr_id.size();
-			id_cid = new HashMap<Integer,Integer>();
+			id_cid = this.getIdCid("/home/infosec/sharding_expt/idcid" + (round - 1) + ".txt");
 		}
+		//注意是LinkedList,记录每个交易内的地址
 		LinkedList<HashSet<Integer>> idsList = new LinkedList<HashSet<Integer>>();
 		for (File blockData : blockDir.listFiles()) {
 			long bstart = System.currentTimeMillis();
@@ -68,8 +72,10 @@ public class BlockProcessor {
 						if (prev_out != null) {
 							String addr = prev_out.getString("addr");
 							if (addr != null) {
-								if(!addr_id.containsKey(addr))
+								if(!addr_id.containsKey(addr)) {
 									addr_id.put(addr, addrsCount++);
+									epochAddrCount++;
+								}
 								tx_ids.add(addr_id.get(addr));
 								if(round > 0) {
 									String utxo = prev_out.getString("tx_index") + " " + addr + prev_out.getString("value")
@@ -92,8 +98,10 @@ public class BlockProcessor {
 						JSONObject out = outs.getJSONObject(k);
 						String addr = out.getString("addr");
 						if (addr != null) {
-							if(!addr_id.containsKey(addr))
+							if(!addr_id.containsKey(addr)) {
 								addr_id.put(addr, addrsCount++);
+								epochAddrCount++;
+							}
 							tx_ids.add(addr_id.get(addr));
 							if(round > 0) {
 								String utxo = out.getString("tx_index") + " " + addr + out.getString("value")
@@ -115,11 +123,12 @@ public class BlockProcessor {
 			long bend = System.currentTimeMillis();
 			System.out.println(bend-bstart);
 		}		
-		System.out.print("This round 参与交易的地址数为:" + addrsCount);
-		//将地址-id映射关系持久化
+		System.out.print("This round 参与交易的地址数为:" + epochAddrCount);
+		//将addr-id映射关系持久化
 		this.saveAddrIdMap(addr_id, "");
 		addr_id.clear();
 		id_cid.clear();
+		//计算本周期的增量边
 		HashMap<int[],Float> edge_weight = new HashMap<int[],Float>();
 		for(Iterator<HashSet<Integer>> idsIterator = idsList.iterator();idsIterator.hasNext();) {
 			HashSet<Integer> ids = idsIterator.next();
@@ -148,11 +157,16 @@ public class BlockProcessor {
 		HashMap<int[],Float> epochGraph = new HashMap<int[],Float>(); 
 		//更新历史图
 		if(round > 0) {
-			BufferedReader ebr = new BufferedReader(new InputStreamReader(new FileInputStream(new File(" "))));
-			BufferedWriter ebw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(" "))));
+			BufferedReader ebr = null;
+			BufferedWriter ebw = null;
+		 try {
+			File preEdgeFile = new File("/home/infosec/sharding_expt/" + "edges" + (round-1) +".txt");
+			File newEdgeFile = new File("/home/infosec/sharding_expt/edges" + round + ".txt");
+			ebr = new BufferedReader(new InputStreamReader(new FileInputStream(preEdgeFile)));
+		    ebw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(newEdgeFile)));
 		    HashMap<int[],Float> remainingEdges = new HashMap<int[],Float>();
 		    String edgeInfo = null;
-		    int edgeCounter = 0;
+		    int edgeCounter = 0;		    
 		    while((edgeInfo = ebr.readLine()) != null) {
 		    	edgeCounter++;
 		    	String[] nodeWeight = edgeInfo.trim().split(" ");
@@ -162,7 +176,10 @@ public class BlockProcessor {
 		    	int[] edge = new int[] {node1,node2};
 		    	if(edge_weight.containsKey(edge)) {
 		    		weight = (float) Math.pow(Math.pow(weight, 0.75)+edge_weight.get(edge),0.75);
-		    		epochGraph.put(edge, weight);
+		    		if(weight > 2)
+		    			epochGraph.put(edge, weight);
+		    		else
+		    			remainingEdges.put(edge, weight);
 		    		edge_weight.remove(edge);
 		    	}else {
 		    		remainingEdges.put(edge, (float) (weight*0.75));
@@ -173,7 +190,6 @@ public class BlockProcessor {
 		    			String new_line = e[0] + " " + e[1] + " " + w;
 		    			ebw.write(new_line);
 		    		}
-		    		epochGraph.clear();
 		    		for(int[] e:remainingEdges.keySet()) {
 		    			float w = remainingEdges.get(e);
 		    			String new_line = e[0] + " " + e[1] + " " + w;
@@ -183,6 +199,26 @@ public class BlockProcessor {
 		    		edgeCounter = 0;
 		    	}
 		    }
+		    preEdgeFile.delete();
+		    for(int[] edges:edge_weight.keySet()) {
+		    	String line = edges[0] + " " + edges[1] + " " + edge_weight.get(edges);
+		    	ebw.write(line);
+		    }
+		    }catch(FileNotFoundException e) {
+		    	e.printStackTrace();
+		    }
+		    
+		    catch(IOException e) {
+		    	e.printStackTrace();
+		    }
+		    finally {
+		    	if(ebr != null) {
+		    		ebr = null;
+		    	}
+		    	if(ebw != null)
+		    		ebw=null;
+		    }
+		    
 		    epochGraph.putAll(edge_weight);
 		}else
 			epochGraph = edge_weight;
@@ -191,11 +227,16 @@ public class BlockProcessor {
 		return epochGraph;
 	}
 	//读取地址-id的映射关系，便于分配地址id
-	public HashMap<String,Integer> getAddrIdMap(String idCounterPath){
+	private HashMap<String,Integer> getAddrIdMap(String idCounterPath){
 		HashMap<String,Integer> addr_id = new HashMap<String,Integer>();
+		FileInputStream fis = null;
+		BufferedReader br = null;
 		try {
-			FileInputStream fis= new FileInputStream(idCounterPath);
-			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+			File idFile = new File("idCounterPath");
+			if(!idFile.exists())
+				idFile.createNewFile();
+			fis= new FileInputStream(idFile);
+			br = new BufferedReader(new InputStreamReader(fis));
 			String line = null;
 			while((line = br.readLine()) != null) {
 				String[] pair = line.split(" ");
@@ -208,14 +249,26 @@ public class BlockProcessor {
 			e.printStackTrace();
 		}catch(IOException e ) {
 			e.printStackTrace();
+		}finally {			
+			try {
+				if(br != null)
+					br.close();
+				if(fis!=null)
+					fis.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}			
 		}
 		return addr_id;
 	}
-	public HashMap<Integer,Integer> getIdCid(String idCidPath){
+	private HashMap<Integer,Integer> getIdCid(String idCidPath){
 		HashMap<Integer,Integer> id_cid = new  HashMap<Integer,Integer>();
+		FileInputStream fis = null;
+		BufferedReader br = null;
 		try {
-			FileInputStream fis= new FileInputStream(idCidPath);
-			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+			fis= new FileInputStream(idCidPath);
+			br = new BufferedReader(new InputStreamReader(fis));
 			String line = null;
 			while((line = br.readLine()) != null) {
 				String[] pair = line.split(" ");
@@ -228,6 +281,16 @@ public class BlockProcessor {
 			e.printStackTrace();
 		}catch(IOException e ) {
 			e.printStackTrace();
+		}finally {			
+			try {
+				if(br != null)
+					br.close();
+				if(fis!=null)
+					fis.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}			
 		}
 		return id_cid;
 	}
